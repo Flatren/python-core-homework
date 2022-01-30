7b4b166e401e00d1c15d08413e41b670b9dddfa6
Flatren <45997535+Flatren@users.noreply.github.com>
2022-01-31 02:14:09 +0500

    Test

---
+++ tests/ex4/__init__.py
@@ -0,0 +1,14 @@
+from data import EMPLOYEES, DEPARTMENTS
+def cross_join(employees, departments):
+    """
+    Реализует декартово произведение списков employees и departments
+
+    :param employees: Список LastName сотрудников таблицы Employee
+    :param departments: Список DepartmentName таблицы Department
+    :return: Генератор пар (LastName, DepartmentName)
+    """
+    for employe in employees:
+        for department in departments:
+            yield (employe, department)
+
+    
