85dca2b42d04dd2b9a9e9a505d4c04e51f993ff4
Flatren <45997535+Flatren@users.noreply.github.com>
2022-01-31 13:40:55 +0500

    1

--- available_actions.py
+++
@@ -1,6 +0,0 @@
-from ex3.action import NothingAction, RockAction, PaperAction, ScissorsAction
-
-NOTHING_ACTION = NothingAction()
-ROCK_ACTION = RockAction()
-PAPER_ACTION = PaperAction()
-SCISSORS_ACTION = ScissorsAction()
