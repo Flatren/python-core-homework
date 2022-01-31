268f4b854bc1ff0485576a99e043abfdc4c4ef58
Flatren <45997535+Flatren@users.noreply.github.com>
2022-01-31 13:41:28 +0500

    Revert "1"
    This reverts commit 85dca2b42d04dd2b9a9e9a505d4c04e51f993ff4.

---
+++ action.py
@@ -0,0 +1,36 @@
+class BaseAction:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self,other):
+        return self.name == other.name
+    def __gt__(self,other):
+        gt_result = False
+        gt_result = gt_result or self.name == 'Paper' and other.name == 'Rock' 
+        gt_result = gt_result or self.name == 'Rock' and other.name == 'Scissors'
+        gt_result = gt_result or self.name == 'Scissors' and other.name == 'Paper'
+        return gt_result
+    def __hash__(self):
+        return hash(self.name)
+    
+class NothingAction(BaseAction):
+    def __init__(self):
+        super().__init__('Nothing')
+        
+    
+class RockAction(BaseAction):
+    def __init__(self):
+        super().__init__('Rock')
+   
+
+class PaperAction(BaseAction):
+    def __init__(self):
+        super().__init__('Paper')
+
+
+class ScissorsAction(BaseAction):
+    def __init__(self):
+        super().__init__('Scissors')
